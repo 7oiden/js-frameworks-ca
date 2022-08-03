aca4ac69fdae3f0f2a1325d4e3aae33d1239b404
Tommy Johnsen <7oiden@gmail.com>
2022-08-03 07:55:18 +0100

    auth fixed

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
-export const WP_BASE_URL = "http://localhost/js-frameworks-ca/wordpress/wp-json/";
+// export const WP_BASE_URL = "http://localhost/js-frameworks-ca/wordpress/wp-json/";
+export const WP_BASE_URL = "https://holidaze.7oiden.com/wp-json/";
 export const TOKEN_PATH = "jwt-auth/v1/token";
 
 const corsFix = "https://noroffcors.herokuapp.com/" 
 
 export const PRODUCTS_URL = corsFix + "https://fakestoreapi.com/products";
 
-////test
